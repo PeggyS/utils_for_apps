d75103757e1dbe98ec4c5fbaffa6ee91daac16ea
PeggyS <peggy.skelly@gmail.com>
2022-07-07 12:17:53 -0400

    add get_db_login_params.m

---
+++ get_db_login_params.m
@@ -0,0 +1,32 @@
+function dbparams = get_db_login_params(dbname)
+% change these values as needed for where the database is running
+
+[~, name] = system('hostname');
+if strncmp(name, 'mini-meg', 8)
+    dbparams.serveraddr = 'localhost';
+elseif contains(name, 'cle-fes')
+    dbparams.serveraddr = '10.83.111.4';
+else
+	% not on a computer on the local network
+	dbparams = [];
+end
+
+switch dbname
+	case 'tdcs_vgait'
+		dbparams.dbname = 'tdcs_vgait';
+		dbparams.user = 'tdcs_vgait';
+		dbparams.password = 'tdcs_vgait';
+	case 'myomo'
+		dbparams.dbname = 'myomo';
+		dbparams.user = 'myomo';
+		dbparams.password = 'user_myomo';
+	case 'clinical_measures'
+		dbparams.dbname = 'clinical_measures';
+		dbparams.user = 'clinmeas';
+		dbparams.password = 'dalyclinmeas';
+	otherwise
+		error('unknown database: %s', dbname)
+end
+
+
+
