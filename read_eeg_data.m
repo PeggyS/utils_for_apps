04044ec9086a06fb16d3d9b0ead367655fcf77e3
PeggyS <peggy.skelly@gmail.com>
2023-07-24 16:13:05 -0400

    Update read_eeg_data.m
    Add sending in the filename as a parameter.

--- read_eeg_data.m
+++ read_eeg_data.m
@@ -1,8 +1,10 @@
-function data = read_eeg_data()
+function data = read_eeg_data(filename)
 % read in Brainvision data
-% output: data is a struct of the eeg data, channels, events
-
-% data structure:
+% input: filename = full path and file name to a *.vhdr BrainVision file. 
+%           It is optional. If no file is sent, the a dialog box asks the user to
+%           identify the file.
+% output: data = a struct of the eeg data, channels, events
+%   data structure:
 %   comments: 'Original file: 20120517_inv_wrist_flexors_active_rc_epochs_450ms.seg' (or .eeg)
 %       srate: 5000				sampling rate in Hz
 %      nbchan: 8				number of channels recorded
@@ -22,16 +24,19 @@ function data = read_eeg_data()
 %     urevent: [1x120 struct]
 %         ref: 'common'
 
-disp('Choose a BrainVision .vhdr file')
-[fname, pname] = uigetfile('*.vhdr', 'Choose a BrainVision .vhdr file');
-if isequal(fname, 0) || isequal(pname,0)
-	disp('User pressed cancel')
-	return
-else
-	filename = fullfile(pname, fname);
-	disp(['Processing ', filename])
+if nargin < 1
+    disp('Choose a BrainVision .vhdr file')
+    [fname, pname] = uigetfile('*.vhdr', 'Choose a BrainVision .vhdr file');
+    if isequal(fname, 0) || isequal(pname,0)
+	    disp('User pressed cancel')
+	    return
+    else
+	    filename = fullfile(pname, fname);
+    end
 end
 
 % read in the data
+disp(['Reading in ', filename])
 [pname, fname, ext] = fileparts(filename);
 [data, ~] = pop_loadbv(pname, [fname ext]);
+
